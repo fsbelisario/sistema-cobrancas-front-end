1a5b2fa965eba9ae3e73f380381f2061ce859b73
Maylla Rabay <mayllarabay@gmail.com>
2021-09-26 16:47:32 -0300

    Biblioteca react-hook-form adicionada, função onSubmit criada, input dos campos registrados

--- src/pages/EnrollUser/index.js
+++ src/pages/EnrollUser/index.js
@@ -1,21 +1,34 @@
 import { Button, TextField } from '@material-ui/core';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import academy from '../../assets/logo-academy.svg';
 import styles from './styles.module.scss';
 
 function EnrollUser() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Conta criada com sucesso!");
+    console.log(data);
+  }
 
   return (
     <div className={styles.content__wrapper}>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <img src={academy} alt="Logo Academy" />
-        <h4>Nome</h4>
-        <TextField className="textField" id="name" variant="standard" required />
-        <h4>E-mail</h4>
-        <TextField id="email" variant="standard" placeholder="exemplo@gmail.com" required />
-        <h4>Senha</h4>
-        <TextField id="password" variant="standard" required />
-        <Button type="submit" variant="contained" disabled={true}>Criar conta</Button>
+        <label>
+          <h4>Nome</h4>
+          <TextField {...register("name", { required: true })} variant="standard" />
+        </label>
+        <label>
+          <h4>E-mail</h4>
+          <TextField {...register("email", { required: true })} placeholder="exemplo@gmail.com" variant="standard" />
+        </label>
+        <label>
+          <h4>Senha</h4>
+          <TextField {...register("password", { required: true })} type="password" variant="standard" />
+        </label>
+        <Button type="submit" variant="contained" disabled={false} className={styles.button__states}>Criar conta</Button>
       </form>
       <footer>Já possui uma conta? <Link to="/">Acesse agora!</Link></footer>
     </div>
