9b81f9f2512bff13fc55dd07e00813d94d096c59
Felipe Belisário <fbelisario@gmail.com>
2021-09-27 18:46:25 -0300

    Revisão das importações do Material UI (atualização da plataforma) e revisão do código.

--- src/components/PasswordInput/index.js
+++ src/components/PasswordInput/index.js
@@ -1,30 +1,39 @@
-import { FormControl, IconButton, Input, InputAdornment } from '@material-ui/core';
-import { Visibility, VisibilityOff } from '@material-ui/icons';
-import React, { useState } from 'react';
+import { useState } from 'react';
+import {
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment
+} from '@mui/material';
+import {
+  Visibility,
+  VisibilityOff
+} from '@mui/icons-material';
 
-
-export function PasswordInput(props) {
+function PasswordInput(props) {
   const [passwordVisible, setPasswordVisible] = useState(false);
 
   return (
     <FormControl>
       <Input
         className={props.className}
-        id={props.id} 
-        type={passwordVisible ? "text" : "password"}
+        id={props.id}
+        type={passwordVisible ? 'text' : 'password'}
         error={props.error}
         {...props.register()}
         endAdornment={
-          <InputAdornment position="end">
-            <IconButton 
-              aria-label="toggle password visibility"
+          <InputAdornment position='end'>
+            <IconButton
+              aria-label='toggle password visibility'
               onClick={() => setPasswordVisible(!passwordVisible)}
             >
               {passwordVisible ? <Visibility /> : <VisibilityOff />}
             </IconButton>
           </InputAdornment>
-        } 
+        }
       />
     </FormControl>
   );
-}
+};
+
+export default PasswordInput;
