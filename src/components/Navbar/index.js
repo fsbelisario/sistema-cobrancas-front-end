7e1a5ae7ef0e364241588d9638865de1fd6719f0
Maylla Rabay <mayllarabay@gmail.com>
2021-09-29 22:12:01 -0300

    Componente Navbar criado e estilizado; Imagem logo-academy-white adicionada

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,44 @@
-import './style.css';
+import billIcon from '../../assets/billing-icon.svg';
+import clientIcon from '../../assets/client-icon.svg';
+import homeIcon from '../../assets/home-icon.svg';
+import academy from '../../assets/logo-academy-white.svg';
+import styles from './styles.module.scss';
+
 
 function Navbar() {
+  const menuItems = [
+    {
+      image: homeIcon,
+      text: 'Home',
+      url: '/home'
+    },
+    {
+      image: billIcon,
+      text: 'Cobranças',
+      url: '#'
+    },
+    {
+      image: clientIcon,
+      text: 'Clientes',
+      url: '#'
+    },
+  ]
 
   return (
-    <>
-
-    </>
+    <div className={styles.Navbar__wrapper}>
+      <img src={academy} alt='Logo Academy' />
+      {menuItems.map((item) => {
+        return(
+          <button>
+            <img src={item.image} alt='' />
+            <p>{item.text}</p>
+          </button>
+        )
+      })}
+      <button className={styles.button__addBilling}>
+        Criar cobrança
+      </button>
+    </div>
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
-import './style.css';
+import Navbar from '../../components/Navbar';
+import styles from './styles.module.scss';
 
 function Home() {
 
   return (
-    <>
-
-    </>
+    <div className={styles.content__wrapper}>
+      <Navbar />
+    </div>
   );
 }
 
