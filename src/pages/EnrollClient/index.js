9462d08e4e4e2c10375ef1ead4a9dbf82d2c583f
Maylla Rabay <mayllarabay@gmail.com>
2021-10-01 15:20:58 -0300

    Estilização do fomulário de cliente concluída

--- src/pages/EnrollClient/index.js
+++ src/pages/EnrollClient/index.js
@@ -1,8 +1,41 @@
+import {
+  Alert,
+  Backdrop,
+  Button,
+  CircularProgress,
+  Snackbar,
+  TextField
+} from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import Navbar from '../../components/Navbar';
 import UserProfile from '../../components/UserProfile';
 import styles from './styles.module.scss';
 
 function EnrollClient() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const [requestError, setRequestError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  function onSubmit() {
+    setLoading(true);
+    console.log('Ainda estou fazendo o formulário');
+    setLoading(false);
+  }
+
+  function handleAlertClose() {
+    setRequestError('');
+  };
+
+  const theme = createTheme({
+    palette: {
+      secondary: {
+        main: '#DA0175'
+      }
+    }
+  });
 
   return (
     <div className={styles.content__wrapper}>
@@ -10,7 +43,210 @@ function EnrollClient() {
       <div className={styles.main__content}>
         <UserProfile />
         <div className={styles.content}>
-          O conteúdo vai aqui!
+          <ThemeProvider theme={theme}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className={styles.input__wrapper}>
+                <label>
+                  {errors.name ? <h4 className={styles.input__error}>Nome</h4> : <h4>Nome</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('name', { required: true })}
+                    id='name'
+                    variant="outlined"
+                    error={!!errors.name}
+                  />
+                  {!!errors.name && <p>O campo Nome é obrigatório!</p>}
+                </label>
+
+                <label>
+                  {errors.email ? <h4 className={styles.input__error}>E-mail</h4> : <h4>E-mail</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('email', { required: true })}
+                    id='email'
+                    variant="outlined"
+                    error={!!errors.email}
+                  />
+                  {!!errors.email && <p>O campo E-mail é obrigatório!</p>}
+                </label>
+              </div>
+              
+              <div className={styles.input__wrapper}>
+                <label>
+                  {errors.tax_id ? <h4 className={styles.input__error}>CPF</h4> : <h4>CPF</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('tax_id', { required: true })}
+                    id='tax_id'
+                    variant="outlined"
+                    error={!!errors.tax_id}
+                  />
+                  {!!errors.tax_id && <p>O campo CPF é obrigatório!</p>}
+                </label>
+
+                <label>
+                  {errors.phone ? <h4 className={styles.input__error}>Telefone</h4> : <h4>Telefone</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('phone', { required: true })}
+                    id='phone'
+                    variant="outlined"
+                    error={!!errors.phone}
+                  />
+                  {!!errors.phone && <p>O campo Telefone é obrigatório!</p>}
+                </label>
+              </div>
+
+              <div className={styles.input__wrapper}>
+                <label>
+                  {errors.zip_code ? <h4 className={styles.input__error}>CEP</h4> : <h4>CEP</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('zip_code')}
+                    id='zip_code'
+                    variant="outlined"
+                    error={!!errors.zip_code}
+                  />
+                </label>
+
+                <label>
+                  {errors.street ? <h4 className={styles.input__error}>Logradouro</h4> : <h4>Logradouro</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('street')}
+                    id='street'
+                    variant="outlined"
+                    error={!!errors.street}
+                  />
+                </label>
+              </div>
+
+              <div className={styles.input__wrapper}>
+                <label>
+                  {errors.number ? <h4 className={styles.input__error}>Número</h4> : <h4>Número</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('number')}
+                    id='number'
+                    variant="outlined"
+                    error={!!errors.number}
+                  />
+                </label>
+
+                <label>
+                  {errors.address_details 
+                    ? <h4 className={styles.input__error}>Complemento</h4> 
+                    : <h4>Complemento</h4>
+                  }
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('address_details')}
+                    id='address_details'
+                    variant="outlined"
+                    error={!!errors.address_details}
+                  />
+                </label>
+              </div>
+
+              <div className={styles.input__wrapper}>
+                <label>
+                  {errors.district ? <h4 className={styles.input__error}>Bairro</h4> : <h4>Bairro</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('district')}
+                    id='district'
+                    variant="outlined"
+                    error={!!errors.district}
+                  />
+                </label>
+                
+                <label>
+                  {errors.reference ? <h4 className={styles.input__error}>Ponto de referência</h4> : <h4>Ponto de referência</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('reference')}
+                    id='reference'
+                    variant="outlined"
+                    error={!!errors.reference}
+                  />
+                </label>
+              </div>
+
+              <div className={styles.input__wrapper}>
+                <label>
+                  {errors.city ? <h4 className={styles.input__error}>Cidade</h4> : <h4>Cidade</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('city')}
+                    id='city'
+                    variant="outlined"
+                    error={!!errors.city}
+                  />
+                </label>
+
+                <label>
+                  {errors.state ? <h4 className={styles.input__error}>Estado</h4> : <h4>Estado</h4>}
+                  <TextField
+                    className={styles.fieldset}
+                    color='secondary'
+                    {...register('state')}
+                    id='state'
+                    variant="outlined"
+                    error={!!errors.state}
+                  />
+                </label>
+              </div>
+              
+              <Snackbar
+                className={styles.snackbar}
+                open={!!requestError}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                autoHideDuration={3000}
+                onClose={handleAlertClose}
+              >
+                <Alert severity='error'>
+                  {requestError}
+                </Alert>
+              </Snackbar>
+
+              <div className={styles.button__wrapper}>
+                <Button 
+                  className={`${styles.button__states} ${styles.button__cancel}`}
+                >
+                  Cancelar
+                </Button>
+                <Button
+                  className={styles.button__states}
+                  type='submit'
+                  disabled={false}
+                  variant='contained'
+                >
+                  Cadastrar Cliente
+                </Button>
+              </div>
+
+              <Backdrop 
+                sx={{
+                  color: 'var(--color-white)',
+                  zIndex: (theme) => theme.zIndex.drawer + 1
+                }}
+                open={loading}
+              >
+                <CircularProgress color='inherit' />
+              </Backdrop>
+            </form>
+          </ThemeProvider>
         </div>
       </div>
     </div>
