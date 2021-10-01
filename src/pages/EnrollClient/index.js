b469a0d738f248da581cec63ed4c423e110519fd
Maylla Rabay <mayllarabay@gmail.com>
2021-10-01 10:31:43 -0300

    Página cadastrar cliente iniciada, estilização base feita

--- src/pages/EnrollClient/index.js
+++ src/pages/EnrollClient/index.js
@@ -1,11 +1,19 @@
-import './style.css';
+import Navbar from '../../components/Navbar';
+import UserProfile from '../../components/UserProfile';
+import styles from './styles.module.scss';
 
 function EnrollClient() {
 
   return (
-    <>
-
-    </>
+    <div className={styles.content__wrapper}>
+      <Navbar />
+      <div className={styles.main__content}>
+        <UserProfile />
+        <div className={styles.content}>
+          O conteúdo vai aqui!
+        </div>
+      </div>
+    </div>
   );
 }
 
